05f54da5c0df2c663121099f52be164cb7e89ddd
guomeng <meng.guo@9-a-m.com>
2018-02-28 18:17:12 +0800

    安装 蓝牙

--- js/AppContent.js
+++ js/AppContent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import ble from 'ble';
 import AppNavigator from './pages';
 
 class AppContent extends Component {
